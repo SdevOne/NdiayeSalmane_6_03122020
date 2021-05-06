d3bcbeb9a64b405bed45a101580850517a735598
Sd <sdevone@outlook.fr>
2021-05-06 14:45:12 +0200

    aaddition of the data processing file

---
+++ public/js/data.js
@@ -0,0 +1,22 @@
+import { photographer } from "./photographer.js";
+import { photographerPage } from "./photographerPage.js";
+
+export class data {
+  static getData() {
+    const data = fetch("./public/js/api/data.json").then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    });
+    return data;
+  }
+
+  static getAllPhotographers() {
+    const allPhotographers = this.getData().then(function (data) {
+      for (const photographersData of data.photographers) {
+        let photographers = new photographer(photographersData);
+        photographers.setCard();
+      }
+    });
+  }
+}
