19261a8c04d45b21e61d92a0f157a62b243f5a80
Sd <sdevone@outlook.fr>
2021-06-14 20:26:40 +0200

    addition of the photographer page title

--- public/js/photographerPage.js
+++ public/js/photographerPage.js
@@ -3,5 +3,7 @@ import { Page } from "./Page.js";
 
 Page.setPhotographerPage();
 const photographerId = location.search.substring(1).split("/")[1];
+const photographerName = location.search.substring(1).split("/")[0].replace("_", " ");
+document.title = photographerName;
 Data.getPhotographer(photographerId);
 Data.getMedias(photographerId);
