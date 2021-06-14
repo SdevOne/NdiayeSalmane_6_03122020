387c97fb79c32d331e8adfa02d92c3733a3cc8e7
Sd <sdevone@outlook.fr>
2021-06-14 19:00:33 +0200

    modification of the photographer class

--- public/js/photographer.js
+++ public/js/photographer.js
@@ -1,113 +1,121 @@
-export class photographer {
-  constructor(object) {
-    this.id = object.id;
-    this.portrait = object.portrait;
-    this.name = object.name;
-    this.city = object.city;
-    this.country = object.country;
-    this.tagline = object.tagline;
-    this.price = object.price;
-    this.tags = object.tags;
+export class Photographer {
+  constructor(photographerData) {
+    this.id = photographerData.id;
+    this.portrait = photographerData.portrait;
+    this.name = photographerData.name;
+    this.city = photographerData.city;
+    this.country = photographerData.country;
+    this.tagline = photographerData.tagline;
+    this.price = photographerData.price;
+    this.tags = photographerData.tags;
   }
-
-  getId() {
-    return this.id;
-  }
-  getPortrait() {
-    return this.portrait;
-  }
-  getName() {
-    return this.name;
-  }
-  getCity() {
-    return this.city;
-  }
-  getCountry() {
-    return this.country;
-  }
-  getTagline() {
-    return this.tagline;
-  }
-  getPrice() {
-    return this.price;
-  }
-  getTags() {
-    return this.tags;
-  }
-
-  setLink(portrait, name) {
+  /**
+   * Mise en place du lien vers la page du photographe
+   * @returns {HTMLElement}
+   */
+  setLink() {
     let link = document.createElement("a");
-    link.classList.add("photographer__identity");
-    link.setAttribute("href", /*name.textContent.replace(/\s+/g, "-")*/ "photographer.html");
-    link.setAttribute("aria-label", name.textContent);
-    link.appendChild(portrait);
-    link.appendChild(name);
+    link.classList.add("photographers__link");
+    link.setAttribute("href", "photographer.html?" + this.name.replace(" ", "_") + "/" + this.id);
+    link.setAttribute("aria-label", this.name);
+    link.setAttribute("tabindex", "4");
+    link.appendChild(this.setPortrait());
+    link.appendChild(this.setName());
     return link;
   }
-
+  /**
+   * Mise en place du portrait
+   * @returns {HTMLElement}
+   */
   setPortrait() {
     let portrait = document.createElement("img");
-    portrait.classList.add("photographer__portrait");
+    portrait.classList.add("photographers__portrait");
     portrait.setAttribute("src", "public/img/PhotographersIDPhotos/" + this.portrait);
-    portrait.setAttribute("alt", "");
+    portrait.setAttribute("alt", this.name);
     return portrait;
   }
-
+  /**
+   * Mise en place du nom
+   * @returns {HTMLElement}
+   */
   setName() {
     let name = document.createElement("h2");
-    name.classList.add("photographer__name");
+    name.classList.add("photographers__name");
     name.textContent = this.name;
     return name;
   }
-
+  /**
+   * Mise en place de la localisation
+   * @returns {HTMLElement}
+   */
   setLocation() {
     let location = document.createElement("p");
-    location.classList.add("photographer__location");
+    location.classList.add("photographers__location");
+    location.setAttribute("tabindex", "4");
+    location.setAttribute("aria-label", "Origine : " + this.city + ", " + this.country);
     location.textContent = this.city + ", " + this.country;
     return location;
   }
-
+  /**
+   * Mise en place de la phrase d'accroche
+   * @returns {HTMLElement}
+   */
   setTagline() {
     let tagline = document.createElement("p");
-    tagline.classList.add("photographer__tagline");
+    tagline.classList.add("photographers__tagline");
+    tagline.setAttribute("tabindex", "4");
+    tagline.setAttribute("aria-label", "Slogan : " + this.tagline);
     tagline.textContent = this.tagline;
     return tagline;
   }
-
+  /**
+   * Mise en place du prix
+   * @returns {HTMLElement}
+   */
   setPrice() {
     let price = document.createElement("p");
-    price.classList.add("photographer__price");
+    price.classList.add("photographers__price");
+    price.setAttribute("tabindex", "4");
+    price.setAttribute("aria-label", "Tarif : " + this.price + "€ par jour");
     price.textContent = this.price + "€/jour";
     return price;
   }
-
+  /**
+   * Mise en place des tags
+   * @returns {HTMLElement}
+   */
   setTags() {
-    let tagContainer = document.createElement("div");
-    tagContainer.classList.add("photographer__tags");
-
+    let ul = document.createElement("ul");
+    ul.classList.add("photographers__tags");
+    ul.setAttribute("aria-label", "Catégorie du photographe");
     let tagData = this.tags;
     for (let data of tagData) {
-      let tag = document.createElement("a");
-      tag.classList.add("nav__link");
-      tag.textContent = "#" + data + " ";
-      let srOnly = document.createElement("span");
+      let tag = document.createElement("li");
+      tag.classList.add("tag");
+      tag.setAttribute("tabindex", "4");
+      tag.setAttribute("aria-label", "Hashtag " + data);
+      tag.textContent = "#" + data;
+      /*let srOnly = document.createElement("span");
       srOnly.classList.add("sr-only");
-      srOnly.textContent = "#" + data + " ";
-      tag.appendChild(srOnly);
-      tagContainer.appendChild(tag);
+      srOnly.textContent = "Hashtag";
+      tag.insertAdjacentElement("beforeend", srOnly);*/
+      ul.appendChild(tag);
     }
-    return tagContainer;
+    return ul;
   }
-
-  setPage() {
-    let div = document.querySelector(".photographer");
+  /**
+   * Création du profil du photographe
+   * @returns {HTMLElement}
+   */
+  setCard() {
+    let parent = document.querySelector(".photographers");
     let card = document.createElement("div");
-    card.classList.add("photographer__profil");
-    card.appendChild(this.setLink(this.setPortrait(), this.setName()));
+    card.classList.add("photographers__profil");
+    card.appendChild(this.setLink());
     card.appendChild(this.setLocation());
     card.appendChild(this.setTagline());
     card.appendChild(this.setPrice());
     card.appendChild(this.setTags());
-    div.appendChild(card);
+    parent.appendChild(card);
   }
 }
